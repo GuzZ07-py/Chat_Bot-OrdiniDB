62cc8766cf29afd93ff23e6d3dfa71fee82cd091
GuzZ07-py <guidogobbetti07@gmail.com>
2026-05-04 20:41:47 +0200

    modifica 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ async function sendMessage() {
   const userMsg = { role: "user", text: input };
   const userid="user123";
   const API_URL=import.meta.env.VITE_API_URL || "http://localhost:8000";
-  const res = await fetch('${API_URL}/chat', {
+  const res = await fetch(`${API_URL}/chat`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
