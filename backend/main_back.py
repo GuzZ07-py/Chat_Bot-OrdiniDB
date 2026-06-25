5ae252b61e1227be37ee758ff19d797c65df366a
GuzZ07-py <guidogobbetti07@gmail.com>
2026-06-25 23:01:03 +0200

    updated model version

--- backend/main_back.py
+++ backend/main_back.py
@@ -176,7 +176,7 @@ TOOLS=[ {
         ]
     }]
 
-model = genai.GenerativeModel(model_name="gemini-1.5-flash",
+model = genai.GenerativeModel(model_name="gemini-2.5-flash",
                               tools=TOOLS,
                               system_instruction=SYSTEM_PROMT)
 
